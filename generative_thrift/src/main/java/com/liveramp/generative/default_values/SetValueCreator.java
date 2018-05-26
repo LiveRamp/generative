340b01021cc31f45d465705425623b836e2e5cfa
Roshan George <rgeorge@liveramp.com>
2018-05-25 18:32:48 -0700

    Recursively inspect to create an arbitrary Set
    Previously, all Sets were assumed to implicitly contain structs, unions
    or primitives. This change recursively inspects the type of the values
    contained in the Set in order to produce values of the correct type so
    long as DefaultValueCreatorFactory can find a creator for that type.

---
+++ generative_thrift/src/main/java/com/liveramp/generative/default_values/SetValueCreator.java
@@ -0,0 +1,28 @@
+package com.liveramp.generative.default_values;
+
+import java.util.Set;
+
+import org.apache.thrift.meta_data.FieldValueMetaData;
+import org.apache.thrift.meta_data.SetMetaData;
+
+import com.liveramp.generative.Arbitrary;
+import com.liveramp.generative.ArbitraryBoundedInt;
+import com.liveramp.generative.SetOf;
+
+public class SetValueCreator implements DefaultValueCreator<Set> {
+
+  private DefaultValueCreatorFactory defaultValueCreatorFactory;
+
+  public SetValueCreator(DefaultValueCreatorFactory defaultValueCreatorFactory) {
+    this.defaultValueCreatorFactory = defaultValueCreatorFactory;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public Arbitrary<Set> apply(FieldValueMetaData fv) {
+    SetMetaData sv = (SetMetaData)fv;
+    DefaultValueCreator<?> elementValueCreator = defaultValueCreatorFactory.apply(sv.elemMetaData);
+    Arbitrary<?> arbitraryElement = elementValueCreator.apply(sv.elemMetaData);
+    return new SetOf(arbitraryElement, new ArbitraryBoundedInt(0, 128));
+  }
+}
