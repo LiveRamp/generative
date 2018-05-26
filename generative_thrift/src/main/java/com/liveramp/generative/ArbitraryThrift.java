e500c077277a5eebb0842cc0c7ae301371404d38
Roshan George <rgeorge@liveramp.com>
2018-05-25 18:46:51 -0700

    Switch to the new DefaultValueCreatorFactory

--- generative_thrift/src/main/java/com/liveramp/generative/ArbitraryThrift.java
+++ generative_thrift/src/main/java/com/liveramp/generative/ArbitraryThrift.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 import org.apache.thrift.TBase;
@@ -20,17 +19,14 @@ import org.apache.thrift.TFieldRequirementType;
 import org.apache.thrift.TUnion;
 import org.apache.thrift.meta_data.FieldMetaData;
 import org.apache.thrift.meta_data.FieldValueMetaData;
-import org.apache.thrift.meta_data.ListMetaData;
-import org.apache.thrift.meta_data.SetMetaData;
-import org.apache.thrift.meta_data.StructMetaData;
-import org.apache.thrift.protocol.TType;
+
+import com.liveramp.generative.default_values.DefaultValueCreatorFactory;
 
 public class ArbitraryThrift<T extends TBase> implements Arbitrary<T> {
 
-
   private final Class<T> clazz;
-  private Map<Byte, DefaultValueCreator> defaultTypeArbitraries;
   private final Map<TFieldIdEnum, Arbitrary<?>> fieldArbitraries;
+  private final DefaultValueCreatorFactory defaultValueCreatorFactory;
 
   public ArbitraryThrift(Class<T> clazz) {
     this(clazz, new HashMap<>());
@@ -39,67 +35,10 @@ public class ArbitraryThrift<T extends TBase> implements Arbitrary<T> {
   public ArbitraryThrift(Class<T> clazz,
                          Map<TFieldIdEnum, Arbitrary<?>> fieldArbitraries) {
     this.clazz = clazz;
-    this.defaultTypeArbitraries = new HashMap<>();
-
-    this.defaultTypeArbitraries.put(TType.BOOL, boolPrim);
-    this.defaultTypeArbitraries.put(TType.BYTE, bytePrim);
-    this.defaultTypeArbitraries.put(TType.I16, shortPrim);
-    this.defaultTypeArbitraries.put(TType.I32, intPrim);
-    this.defaultTypeArbitraries.put(TType.DOUBLE, doublePrim);
-    this.defaultTypeArbitraries.put(TType.STRING, stringPrim);
-    this.defaultTypeArbitraries.put(TType.I64, longPrim);
-    this.defaultTypeArbitraries.put(TType.SET, setPrim);
-    this.defaultTypeArbitraries.put(TType.STRUCT, structPrim);
     this.fieldArbitraries = new TreeMap<>(new FieldIdComparator());
     this.fieldArbitraries.putAll(fieldArbitraries);
+    this.defaultValueCreatorFactory = new DefaultValueCreatorFactory();
   }
-
-  public static final DefaultValueCreator<Boolean> boolPrim = (m, fv) -> new ArbitraryBoolean();
-  public static final DefaultValueCreator<Byte> bytePrim =
-      (m, fv) -> new ArbitraryByteArray(1).map(b -> b[0]);
-  public static final DefaultValueCreator<Short> shortPrim =
-      (m, fv) -> new ArbitraryBoundedInt(Short.MIN_VALUE, Short.MAX_VALUE).map(i -> i.shortValue());
-  public static final DefaultValueCreator<Integer> intPrim =
-      (m, fv) -> new ArbitraryBoundedInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
-  public static final DefaultValueCreator<Long> longPrim =
-      (m, fv) -> (r) -> r.nextLong();
-  public static final DefaultValueCreator<Double> doublePrim =
-      (m, fv) -> (r) -> r.nextDouble();
-  public static final DefaultValueCreator stringPrim =
-      (m, f) -> {
-        FieldValueMetaData fv = (FieldValueMetaData)f;
-        ArbitraryBoundedInt size = new ArbitraryBoundedInt(1, 256);
-        if (fv.isBinary() || fv.getTypedefName() != null) {
-          return size.flatMap(
-              l -> new ArbitraryByteArray(l),
-              s -> s.length).map(
-              b -> ByteBuffer.wrap(b),
-              bb -> bb.array()
-          );
-        } else {
-          return size.flatMap(
-              l -> new ArbitraryString(l),
-              s -> s.length());
-        }
-      };
-
-  public static final DefaultValueCreator structPrim =
-      (m, fv) -> new ArbitraryThrift(((StructMetaData)fv).structClass, new HashMap<>());
-  public static final DefaultValueCreator<Set> setPrim =
-      (m, fv) -> {
-        SetMetaData setMetaData = (SetMetaData)fv;
-        DefaultValueCreator<?> constructor = m.get(setMetaData.elemMetaData.type);
-        Arbitrary<?> elems = constructor.apply(m, setMetaData.elemMetaData);
-        return new SetOf(elems, new ArbitraryBoundedInt(0, 128));
-      };
-
-  public static final DefaultValueCreator<List> listPrim =
-      (m, fv) -> {
-        ListMetaData listMetaData = (ListMetaData)fv;
-        DefaultValueCreator<?> constructor = m.get(listMetaData.elemMetaData.type);
-        Arbitrary<?> elems = constructor.apply(m, listMetaData.elemMetaData);
-        return new ListOf(elems, new ArbitraryBoundedInt(0, 128));
-      };
 
   @Override
   public T get(Random r) {
@@ -171,15 +110,12 @@ public class ArbitraryThrift<T extends TBase> implements Arbitrary<T> {
 
 
   private Optional<Arbitrary> getArbitrary(Map.Entry<? extends TFieldIdEnum, FieldMetaData> entry) {
-    byte type = entry.getValue().valueMetaData.type;
+    FieldValueMetaData vm = entry.getValue().valueMetaData;
     TFieldIdEnum key = entry.getKey();
     if (fieldArbitraries.containsKey(key)) {
       return Optional.of(fieldArbitraries.get(key));
-    } else if (defaultTypeArbitraries.containsKey(type)) {
-      DefaultValueCreator<?> valueCreator = defaultTypeArbitraries.get(type);
-      return Optional.of(valueCreator.apply(defaultTypeArbitraries, entry.getValue().valueMetaData));
     } else {
-      return Optional.empty();
+      return Optional.of(defaultValueCreatorFactory.apply(vm).apply(vm));
     }
   }
 
@@ -202,7 +138,4 @@ public class ArbitraryThrift<T extends TBase> implements Arbitrary<T> {
     }
   }
 
-  private interface DefaultValueCreator<T> extends BiFunction<Map<Byte, DefaultValueCreator>, FieldValueMetaData, Arbitrary<T>> {
-
-  }
 }
